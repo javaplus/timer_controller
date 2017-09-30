b475b8c06bfd5a3ce7dd8036e1311edcc01f3634
Barry Tarlton <TARLTOB1@nationwide.com>
2017-09-30 16:28:07 -0400

    Adding server to use MQTT

---
+++ server.py
@@ -0,0 +1,51 @@
+from flask import Flask, request, json
+import logging
+import sys
+import timer
+import paho.mqtt.client as mqtt
+
+client = mqtt.Client()
+
+#from timer import stopCountDown
+
+logging.basicConfig(level=logging.DEBUG)
+app = Flask(__name__)
+
+@app.route('/')
+def api_pitimer():
+    return 'Hello World'
+
+@app.route('/stop')
+def api_stop():
+    timer.stopCountDown()
+    return 'stopping'
+
+@app.route('/timer', methods = ['POST'])
+def api_timer():
+    logging.basicConfig(level=logging.DEBUG)
+    logging.info('Data=' + request.data)
+    logging.info(request.json["minutes"])
+    mqttc = mqtt.Client("timer_pub")
+    mqttc.connect("localhost", 1883)
+    mqttc.publish("timer/timer", request.data)
+    mqttc.loop(2) #timeout = 2s
+
+    #timer.countDown(int(request.json["minutes"])) 
+
+
+    return 'Processing Timer'
+
+if __name__ == '__main__':
+    pitimer = logging.getLogger("flask.app")
+    pitimer.setLevel(logging.WARN)
+    adaLogger = logging.getLogger("Adafruit_I2C")
+    adaLogger.setLevel(logging.WARN)
+
+
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    pitimer.addHandler(ch)
+    #logging.basicConfig(stream=sys.stdout)
+    app.run(host='0.0.0.0')
